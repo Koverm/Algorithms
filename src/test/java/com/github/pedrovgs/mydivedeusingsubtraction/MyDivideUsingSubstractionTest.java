83691b8140a2169f255b2630d64c0f785f8411d4
kover <koverm0414@gmail.com>
2017-10-08 18:07:18 +0200

    MyDivideUsingSubstractionTest added

---
+++ src/test/java/com/github/pedrovgs/mydivedeusingsubtraction/MyDivideUsingSubstractionTest.java
@@ -0,0 +1,54 @@
+package com.github.pedrovgs.mydivedeusingsubtraction;
+
+import com.github.pedrovgs.problem5.DivideUsingSubtraction;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyDivideUsingSubstractionTest {
+
+    private static final float DELTA=0.1f;
+
+    private DivideUsingSubtraction dus;
+
+    @Before
+    public void setUp(){dus= new DivideUsingSubtraction();}
+
+    @Test (expected = IllegalArgumentException.class)
+    public void DivideByZeroIterative(){
+        dus.divideIterative(25,0);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void DivideByZeroRecursive(){
+        dus.divideRecursive(25,0);
+    }
+    @Test
+    public void DividableRecursive(){
+        float result =dus.divideRecursive(100,25);
+
+        assertEquals(4, result, DELTA);
+    }
+
+    @Test
+    public void DividableIterative(){
+        float result =dus.divideIterative(200,25);
+
+        assertEquals(8, result, DELTA);
+    }
+    @Test
+    public void NonDividableRecursive(){
+        float result =dus.divideRecursive(110,20);
+
+        assertEquals(5.5f, result, DELTA);
+    }
+
+    @Test
+    public void NonDividableIterative(){
+        float result =dus.divideIterative(113,10);
+
+        assertEquals(11.3, result, DELTA);
+    }
+
+}
